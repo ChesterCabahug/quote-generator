3dd5917d210c359e5e6059be9ba538d857fe4803
Chester <chesterdeveloper2000@gmail.com>
2020-07-22 13:01:39 +0800

    added quote to the html, also made the tweet and new quote button work!! 😍

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
+const quoteContainer = document.getElementById("quote-container")
+const quoteText = document.getElementById("quote")
+const authorText = document.getElementById("author")
+const twitterBtn = document.getElementById("twitter")
+const newQuoteBtn = document.getElementById("new-quote")
+
 // get quote from API
-async function getQuote() {
+getQuote = async () => {
     const proxyUrl = "https://cors-anywhere.herokuapp.com/"
     const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
     try {
         const response = await fetch(proxyUrl + apiUrl)
         const data = await response.json()
+        
         console.log(data)
+
+        // if author is blank, add unknown
+        if (data.quoteAuthor === "") {
+            authorText.innerText = "Unknown"
+        } else {
+            authorText.innerText = data.quoteAuthor
+        }
+
+        // reduced font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add("long-quote")
+        } else {
+            quoteText.classList.remove("long-quote")
+        }
+        quoteText.innerText = data.quoteText
+
     } catch (error) {
+        console.log(error)
         getQuote()
-        console.log("whoops, no quote", error)
     }
 }
+
+
+tweetQuote = () => {
+    const quote = quoteText.innerText
+    const author = authorText.innerText
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+
+    window.open(twitterUrl, "_blank")
+}
+
+
+// event listeners
+newQuoteBtn.addEventListener("click", getQuote)
+twitterBtn.addEventListener("click", tweetQuote)
+
 
 // on load
 getQuote()
